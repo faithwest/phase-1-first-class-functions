77939b080cb1293487feee4c1dff44abaa02c285
faithwest <faithchepkoech425@gmail.com>
2023-09-22 15:54:31 +0300

    complete

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(spy) {
+    spy();
+  }
+
+  function returnsANamedFunction() {
+    function namedFunction() {
+ }
+        return namedFunction;
+  }
+
+  function returnsAnAnonymousFunction () {
+    return function() {
+    }
+  };
